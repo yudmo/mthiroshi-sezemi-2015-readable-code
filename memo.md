eebb2ed77ed1d068a5bc5be5757f0535bdfe02f8
Yudai MORIYA <s1520699@u.tsukuba.ac.jp>
2015-06-06 14:43:21 +0900

    仕様5を実装

--- memo.md
+++ memo.md
@@ -2,3 +2,21 @@
 #define SIZEREAD 256 /* 1度に読み込むサイズ */
 #define SIZELIEN 100 /* 読み込む行数のサイズ */
 
+##午後
+実際のコード
+char read_data[N] = {'\0'}; /* 読み込んだデータを格納用 */
+char **data_list; /* 読み込んだデータを格納する配列 */
+
+理由
+コメントが多く書かれていてリーダブル
+
+ほとんどすべてに処理がわかるようにコメントがされていた．
+
+実際のコード
+if ((fp = fopen(argv[1], "r")) == NULL) { /* ファイルオープンのエラーチェック*/
+  printf("FILE OPEN ERROR\n");
+  exit(EXIT_FAILURE);
+}
+
+理由
+exit前にメッセージを表示しているのでリーダブル
--- recipe.c
+++ recipe.c
@@ -1,37 +1,43 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define SIZEREAD 256 /* 1度に読み込むサイズ */
-#define SIZELIEN 100 /* 読み込む行数のサイズ */
+#define SIZELINE 100 /* 読み込む行数のサイズ */
 
 int main(int argc, char* argv[]){
-    FILE *fp;
-    char read_data[N] = {'\0'}; /* 読み込んだデータを格納用 */
+  FILE *fp;
+  char read_data[SIZEREAD] = {'\0'}; /* 読み込んだデータを格納用 */
+  int i,j;/*ループ変数*/
 
-    char **data_list; /* 読み込んだデータを格納する配列 */
+  char data_list[256][256]; /* 読み込んだデータを格納する配列 */
+  for(i=0 ; i < 256 ; i++){/*初期化*/
+    for(j=0 ; i < 256 ; i++){
+      data_list[i][j] = NULL;
+    }
+  }
 
-    if (argc < 2) { /* 引数エラーチェック */
-        printf("Usage Error");
-        exit(EXIT_FAILURE);
+  if (argc < 2) { /* 引数エラーチェック */
+    printf("Usage Error");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fp = fopen(argv[1], "r")) == NULL) { /* ファイルオープンのエラーチェック*/
+    printf("FILE OPEN ERROR\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /* ファイルの終わりまでループ */
+  for(i = 0;i < SIZELINE && fgets(read_data, SIZEREAD, fp) != NULL; i++) { /* 1行ずつ読み込み */
+
+    for(j = 0; j < strlen(read_data); j++){
+      data_list[i][j] = read_data[j];
     }
 
-    if ((fp = fopen(argv[1], "r")) == NULL) { /* ファイルオープンのエラーチェック*/
-        printf("FILE OPEN ERROR\n");
-        exit(EXIT_FAILURE);
-    }
+    printf("%d:%s",i+1, data_list[i]);/* 読み込んだデータを表示 */
+  }
 
-    data_list = malloc(sizeof(char) * SIZELINE);
-    /* ファイルの終わりまでループ */
-    for(i = 0;i < SIZELINE || fgets(read_data, SIZEREAD, fp) != NULL; i++) { /* 1行ずつ読み込み */
+  fclose(fp); /* ファイルのクローズ */
 
-        /* 開発途中のためコメントアウト */
-        // data_list = malloc(sizeof(char) * strlen(read_data)):
-
-        printf("%s", read_data);/* 読み込んだデータを表示 */
-    }
-
-    fclose(fp); /* ファイルのクローズ */
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
